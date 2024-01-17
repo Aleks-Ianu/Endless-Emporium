785c866b75420101ebcb08c9ce4595270948d247
jsneath <jamessneath1@gmail.com>
2024-01-17 20:57:23 +0000

    push working navbar with menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import FakeStoreComponent from './components/FakeStoreComponent'
+import Navbar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <FakeStoreComponent />
+      
     </>
   )
 }
