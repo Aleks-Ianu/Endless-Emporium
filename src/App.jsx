a145c058890686bef478d0e9ba3f63c151c8bbfc
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-18 20:00:02 +0000

    adds the jumbrotron component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 import FakeStoreComponent from './components/FakeStoreComponent'
 import Navbar from './components/NavBar'
+import Jumbotron from './components/Jumbotron'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
       <FakeStoreComponent />
-      
     </>
   )
 }
