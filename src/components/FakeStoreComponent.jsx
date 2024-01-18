6c983ac7a6ca5a88b1788cc414f3aaec3036dacd
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2024-01-18 19:38:20 +0000

    FakeStoreComponent added

--- src/components/FakeStoreComponent.jsx
+++ src/components/FakeStoreComponent.jsx
@@ -1,10 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const FakeStoreComponent = () => {
+const FakeStoreProductComponent = () => {
   const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get('https://fakestoreapi.com/products/');
+        setProducts(response.data);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    };
+  
+    fetchProducts();
+  }, []);
+  
+  return (
+    <div>
+      <h2>FakeStore Products</h2>
+      {products.map(product => (
+  <div key={product.id}>
+    <ul>
+    <img src={product.image} alt="product image" />
+    <li key={product.id}>{product.title}</li>
+    </ul>
+  </div>
+  ))}
+  
+    </div>
+  );
 
 }
 
-export default FakeStoreComponent;
+export default FakeStoreProductComponent;
 
