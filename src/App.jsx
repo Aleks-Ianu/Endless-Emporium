5ea6fb95da67793fe9ad888ea43ecf14c49e1c9f
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2024-01-18 19:53:25 +0000

    Merged conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import FakeStoreComponent from './components/FakeStoreComponent'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import FakeStoreComponent from './components/FakeStoreComponent';
 import Navbar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
-
+   
   return (
-    <>
-      <Navbar />
-      <FakeStoreComponent />
-      
-    </>
+    <Router>
+      <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<div><Navbar /><FakeStoreComponent /></div>} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
