fdfeab3488857c23d67e098452ff6eb5e9f9c2dd
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2024-01-20 15:26:16 +0000

    Created ChatBot folder and LinkList and ChatOptions folder inside, created various component to show a chatbox on the product page that has initial functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ import Product from './components/Product';
 <co></co>
 import HomePage from './pages/HomePage'
 import NavBar from './components/Navbar';
+// Chatbot imports
+import Chatbot from 'react-chatbot-kit';
+import ActionProvider from './components/Chatbot/ActionProvider';
+import MessageParser from './components/Chatbot/MessageParser';
+import config from './components/Chatbot/Config';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,7 +20,7 @@ function App() {
       <Routes>
       {/* <Route path="/" element={<Login />} /> */}
       <Route path="/" element= {<HomePage />} />
-      <Route path="/product/:productId" element={<div><NavBar /><Product /></div>} />
+      <Route path="/product/:productId" element={<div><NavBar /><Product /><Chatbot config={config} actionProvider={ActionProvider} 	    messageParser={MessageParser} /></div>} />
 
       </Routes>
     </Router>
