664909acb26d8641f2dbaa0dcf66f5dc41df404b
jsneath <jamessneath1@gmail.com>
2024-01-24 19:24:21 +0000

    cart now doesnt reset when clicking home page

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,11 +1,20 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export const useCart = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(() => {
+    const savedCartItems = localStorage.getItem('cartItems');
+    return savedCartItems ? JSON.parse(savedCartItems) : [];
+  });
+  
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+  }, [cartItems]);
+
+
 
   const addToCart = (product) => {
     setCartItems(currentItems => [...currentItems, product]);
