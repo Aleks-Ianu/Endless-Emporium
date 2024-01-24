dd5b69197004659225b609930ce2e00fd6990d10
Sidiq Hersi <hersisidiq@gmail.com>
2024-01-24 19:19:10 +0000

    adds code that can close the chatbot and also re-open

--- src/components/Chatbot/ChatBotContainer/ChatBotContainer.jsx
+++ src/components/Chatbot/ChatBotContainer/ChatBotContainer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Chatbot from 'react-chatbot-kit';
 import config from '../Config';
 import MessageParser from '../MessageParser';
@@ -6,13 +6,34 @@ import ActionProvider from '../ActionProvider';
 import './ChatBotContainer.css';
 
 const ChatBotContainer = () => {
+  const [isChatbotOpen, setChatbotOpen] = useState(true);
+
+  const toggleChatbot = () => {
+    setChatbotOpen(!isChatbotOpen);
+  };
+
+  const minimizeChatbot = () => {
+    setChatbotOpen(false);
+  };
+
+  const maximizeChatbot = () => {
+    setChatbotOpen(true);
+  };
+
   return (
-    <div className="chatbot-container">
-      <Chatbot
-        config={config}
-        messageParser={MessageParser}
-        actionProvider={ActionProvider}
-      />
+    <div className={`chatbot-container ${isChatbotOpen ? 'open' : 'closed'}`}>
+      <div className="flex justify-between">
+        <button className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full align al hover:bg-blue-700 hover:hover:duration-300" onClick={toggleChatbot}>
+          {isChatbotOpen ? 'Close' : 'Tech Chatbot'}
+        </button>
+      </div>
+      {isChatbotOpen && (
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
+      )}
     </div>
   );
 };
