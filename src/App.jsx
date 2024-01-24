7e1071410dd4ceceb891045c913c667f7ea5bb40
Aleks Ianu <aleksianu@Anas-iMac-2.broadband>
2024-01-24 19:41:16 +0000

    Trying fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter  as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Product from './components/Product';
 <co></co>
