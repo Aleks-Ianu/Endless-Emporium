023ab37e0b317a6b15aa82a66f6d02e29958739b
jsneath <jamessneath1@gmail.com>
2024-01-24 22:12:01 +0000

    adjusted card error

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -29,8 +29,8 @@
     
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-8 ">
               {products.map((product) => (
-                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
-                <a key={product.id} onClick={() => navigate(`/product/${product.id}`)} className="group">
+                <div key={product.id}className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                <a onClick={() => navigate(`/product/${product.id}`)} className="group">
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                     <img
                       src={product.image}
