be1bc7e9a8a97b887ed09964321c491926654fdd
jsneath <jamessneath1@gmail.com>
2024-01-25 18:50:05 +0000

    added extra styling

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -111,6 +111,7 @@ export default function ShoppingCart() {
                       <Link
                           to="/checkout"
                           className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                          onClick={() => setOpen(false)}
                       >
                           Checkout
                       </Link>
